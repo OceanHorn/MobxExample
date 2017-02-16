f7315be8d71cf09928cd293cc32792f7a4fe06e0
GuoYufu <GuoYufu@picclife.cn>
2017-02-16 13:05:45 +0800

    [更新]更新文件

---
+++ index.ios.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import { AppRegistry } from 'react-native';
+
+// @todo remove when RN upstream is fixed
+console.ignoredYellowBox = ['Warning: Failed propType: SceneView'];
+
+import Example from './Example';
+
+AppRegistry.registerComponent('MobxExample', () => Example);
+
+// http://brooch.me/2016/11/23/MobX-simple-entry-1/
+// http://brooch.me/2016/11/29/MobX-simple-entry-2/
+// http://brooch.me/2016/12/09/MobX-simple-entry-3/
+// http://brooch.me/2016/12/16/MobX-simple-entry-4/
